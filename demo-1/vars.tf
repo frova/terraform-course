1682141775cf3e4476a1feeb866b88ea5bb26b02
Franco Rovaretti <francorovaretti@gmail.com>
2020-06-23 00:13:58 -0300

    first commit

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,15 +5,15 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-088cb86eea6186ffe"
+    us-west-2 = "ami-0a4df59262c92cf19"
+    us-east-2 = "ami-079013f7b97c7db32"
   }
 }
 
